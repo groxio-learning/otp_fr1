f2fdd67a00db35a97e9d290620277b8bd7a0ee12
tom milburn <tom@frame.io>
2022-01-11 13:55:31 -0500

    updating tests to be more elixir-ey

--- puzzler/test/puzzler_test.exs
+++ puzzler/test/puzzler_test.exs
@@ -1,26 +1,29 @@
 defmodule PuzzlerTest do
+
+  import Puzzler.Game.Board
+
   use ExUnit.Case
-  doctest Puzzler
 
   test "greets the world" do
-    assert Puzzler.Game.new([1, 2, 3, 4]) ==
-      %{answer: [1, 2, 3, 4], guesses: []}
+    assert new([1, 2, 3, 4]) ==
+      %Puzzler.Game.Board{answer: [1, 2, 3, 4], guesses: []}
   end
 
   test "renders a row" do
     answer = [1, 2, 3, 4]
     guess = [1, 2, 3, 4]
-    assert Puzzler.Game.render_row(answer, guess) == "1 2 3 4 | rrw"
+    assert render_row(answer, guess) == "1 2 3 4 | rrw"
   end
 
   test "renders multiple incorrect guesses" do
-    answer = [10, 11, 12, 13]
-    board = Puzzler.Game.new(answer)
-    board = Puzzler.Game.add(board, [1, 2, 3, 4])
-    board = Puzzler.Game.add(board, [3, 4, 5, 6])
+    actual =
+      [10, 11, 12, 13]
+      |> new()
+      |> add([1, 2, 3, 4])
+      |> add([3, 4, 5, 6])
+      |> render()
 
-    rendered_board = Puzzler.Game.render(board)
-    assert rendered_board ==
+    assert actual ==
     """
     3 4 5 6 | rrw
     1 2 3 4 | rrw
@@ -29,13 +32,13 @@ defmodule PuzzlerTest do
     """
   end
 
-  test "renders a winning game" do
-    answer = [3, 4, 5, 6]
-    board = Puzzler.Game.new(answer)
-    board = Puzzler.Game.add(board, [1, 2, 3, 4])
-    board = Puzzler.Game.add(board, [3, 4, 5, 6])
+  test "renders a winning game.Board" do
+    rendered_board =
+      new([3, 4, 5, 6])
+      |> add([1, 2, 3, 4])
+      |> add([3, 4, 5, 6])
+      |> render()
 
-    rendered_board = Puzzler.Game.render(board)
     assert rendered_board ==
     """
     3 4 5 6 | rrw
@@ -45,21 +48,29 @@ defmodule PuzzlerTest do
     """
   end
 
-  test "renders a losing game" do
-    answer = [3, 4, 5, 6]
-    board = Puzzler.Game.new(answer)
-    board = Puzzler.Game.add(board, [1, 2, 3, 4])
-    board = Puzzler.Game.add(board, [1, 2, 3, 4])
-    board = Puzzler.Game.add(board, [1, 2, 3, 4])
-    board = Puzzler.Game.add(board, [1, 2, 3, 4])
-    board = Puzzler.Game.add(board, [1, 2, 3, 4])
-    board = Puzzler.Game.add(board, [1, 2, 3, 4])
-    board = Puzzler.Game.add(board, [1, 2, 3, 4])
-    board = Puzzler.Game.add(board, [1, 2, 3, 4])
+  test "renders a losing game.Board" do
 
-    rendered_board = Puzzler.Game.render(board)
+    rendered_board =
+      new([3, 4, 5, 6])
+      |> add([1, 2, 3, 4])
+      |> assert_key(:answer, [3, 4, 5, 6])
+      |> assert_key(:guesses, [[1, 2, 3, 4]])
+      |> add([1, 2, 3, 4])
+      |> assert_key(:guesses, [[1, 2, 3, 4], [1, 2, 3, 4]])
+      |> add([1, 2, 3, 4])
+      |> add([1, 2, 3, 4])
+      |> add([1, 2, 3, 4])
+      |> add([1, 2, 3, 4])
+      |> add([1, 2, 3, 4])
+      |> add([1, 2, 3, 4])
+      |> add([1, 2, 3, 4])
+      |> add([1, 2, 3, 4])
+      |> render()
+
     assert rendered_board ==
     """
+    1 2 3 4 | rrw
+    1 2 3 4 | rrw
     1 2 3 4 | rrw
     1 2 3 4 | rrw
     1 2 3 4 | rrw
@@ -72,4 +83,60 @@ defmodule PuzzlerTest do
     sorry, you've lost!
     """
   end
+
+
+  test "renders a losing game, but now bruce is involved" do
+
+    rendered_board =
+      new([3, 4, 5, 6])
+      |> add([1, 2, 3, 4])
+      |> assert_key(:answer, [3, 4, 5, 6])
+      |> assert_key(:guesses, [[1, 2, 3, 4]])
+      |> add([1, 2, 3, 4])
+      |> assert_key(:guesses, [[1, 2, 3, 4], [1, 2, 3, 4]])
+      |> add([1, 2, 3, 4])
+      |> add([1, 2, 3, 4])
+      |> add([1, 2, 3, 4])
+      |> add([1, 2, 3, 4])
+      |> add([1, 2, 3, 4])
+      |> add([1, 2, 3, 4])
+      |> add([1, 2, 3, 4])
+      |> add([1, 2, 3, 4])
+      |> render()
+
+    assert rendered_board ==
+    """
+    1 2 3 4 | rrw
+    1 2 3 4 | rrw
+    1 2 3 4 | rrw
+    1 2 3 4 | rrw
+    1 2 3 4 | rrw
+    1 2 3 4 | rrw
+    1 2 3 4 | rrw
+    1 2 3 4 | rrw
+    1 2 3 4 | rrw
+    1 2 3 4 | rrw
+
+    sorry, you've lost!
+    """
+  end
+
+  test "losing board" do
+    board = losing_board()
+    assert render(board) =~ ~r/sorry/
+  end
+
+  def assert_key(board, key, expected) do
+    actual = Map.get(board, key)
+    assert actual == expected
+
+    board
+  end
+
+  def losing_board() do
+    Stream.repeatedly(fn -> [1, 2, 3, 4] end)
+    |> Enum.take(10)
+    |> Enum.reduce(new([3, 4, 5, 6]), &add(&2, &1))
+  end
+
 end
