8c48fb27bc55b33d90db740c342feb4b08376c14
Leah Goldberg <leahgoldberg31@gmail.com>
2022-01-11 11:30:27 -0500

    Add score module to calculate reds and whites

---
+++ puzzler/lib/puzzler/score.ex
@@ -0,0 +1,29 @@
+defmodule Puzzler.Score do
+  defstruct [:reds, :whites]
+
+  def new(correct_solution, guess) do
+    red_count = count_reds(correct_solution, guess)
+
+    %__MODULE__{
+      reds: red_count,
+      whites: count_whites(correct_solution, guess, red_count)
+    }
+  end
+
+  def render(score) do
+    "rrw"
+  end
+
+  def count_reds(correct_solution, guess) do
+    correct_solution
+    |> Enum.zip(guess)
+    |> Enum.filter(fn {correct, guess} -> correct == guess end)
+    |> Enum.count()
+  end
+
+  def count_whites(correct_solution, guess, red_count) do
+    miss_count = Enum.count(guess -- correct_solution)
+    total_columns = Enum.count(correct_solution)
+    total_columns - red_count - miss_count
+  end
+end
